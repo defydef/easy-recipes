a1fec6ed586b7851b9b334739dc37e0da90d0478
defy <8841170+defydef@users.noreply.github.com>
2023-10-27 08:47:38 +0700

    Show only 5 items on first load, show Load more button

--- src/ui/Recipes.jsx
+++ src/ui/Recipes.jsx
@@ -1,15 +1,28 @@
+import { useState } from "react";
 import { useRecipe } from "../features/recipes/RecipeContext";
 import Recipe from "./Recipe";
 
 function Recipes() {
   const { recipes } = useRecipe();
+  const [maxDisplayedItems, setMaxDisplayedItems] = useState(5);
+  const displayedRecipes =
+    recipes.length > maxDisplayedItems
+      ? recipes.slice(0, maxDisplayedItems)
+      : recipes;
+  const isAllItemsDisplayed = recipes.length === displayedRecipes.length;
+
   return (
     <div className="p-7 flex flex-col gap-3">
       <h1 className="font-bold text-xl mb-5">Recipes for you</h1>
       <article className="grid md:grid-cols-3 md:gap-6 grid-cols-2 gap-3">
-        {recipes.map((recipe) => (
+        {displayedRecipes.map((recipe) => (
           <Recipe key={recipe.id} recipe={recipe} />
         ))}
+        {!isAllItemsDisplayed && (
+          <button onClick={() => setMaxDisplayedItems(maxDisplayedItems + 5)}>
+            Load more...
+          </button>
+        )}
       </article>
     </div>
   );
